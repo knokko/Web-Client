947ef90aa8a7caa0d7f2ea3589ca8bdbc351b1b5
knokko <knokogator@hotmail.com>
2019-01-27 19:55:08 +0100

    Big clean-up and add encryptor/decryptor

--- Connection.js
+++ Connection.js
@@ -2,80 +2,86 @@ const WebClient = {
 	Connection : function(ip, port, onMessage, onDisconnect, onOpen, onFailConnect){
 		this.onMessage = onMessage;
 		this.onOpen = onOpen;
-		this.isOpen = function(){
-			return this.queue === null && !this.closed;
-		}
+		this.onDisconnect = onDisconnect;
+		this.onFailConnect = onFailConnect;
 		this.closed = false;
-		this.sendBytes = function(array){
-			if(this.queue == null)
-				this.socket.send(array.buffer);
-			else
-				this.queue.push(array.buffer);
-		};
-		this.send = function(array){
-			let stringLength = Math.floor(array.length / 2);
-			if(stringLength * 2 != array.length)
-				stringLength++;
-			const array16 = new Uint16Array(stringLength);
-			for(let index = 0; index < stringLength - 1; index++){
-				array16[index] = 256 * array[index * 2] + array[index * 2 + 1];
-			}
-			if(this.queue == null)
-				this.socket.send(String.fromCharCode.apply(String, array16));
-			else
-				this.queue.push(String.fromCharCode.apply(String, array16));
-		};
-		this.createOutput = function(){
-			const thisClient = this;
-			return new BitHelper.ByteArrayBitOutput(undefined, undefined, function(){
-				const arrayToSend = new Int8Array(this.boolIndex === 0 ? this.index : this.index + 1);
-				javaArrayCopy(this.array, 0, arrayToSend, 0, arrayToSend.length);
-				thisClient.sendBytes(arrayToSend);
-			});
-		};
-		this.close = function(){
-			this.socket.close();
-		};
-		
-		let socketOnopen = function(event){
-			for(let index = 0; index < this.queue.length; index++){
-				this.socket.send(this.queue[index]);
-			}
-			this.queue = null;
-			if(this.onOpen){
-				this.onOpen(event);
-			}
-		};
-		socketOnopen = socketOnopen.bind(this);
-		
-		let socketOnmessage = function(event){
-			const fileReader = new FileReader();
-			const thisConnection = this;
-			fileReader.onload = function(event2) {
-				thisConnection.onMessage(new BitHelper.ByteArrayBitInput(new Int8Array(event2.target.result)));
-			};
-			fileReader.readAsArrayBuffer(event.data);
-		};
-		socketOnmessage = socketOnmessage.bind(this);
-		
-		let socketOnDisconnect = function(event){
-			if(this.isOpen()){
-				onDisconnect(event);
-			}
-			else if(onFailConnect){
-				onFailConnect(event);
-			}
-			this.closed = true;
-		};
-		socketOnDisconnect = socketOnDisconnect.bind(this);
-		
-		this.queue = [];
-		
+		this.encryptor = null;
+		this.decryptor = null;
+		this.binaryQueue = [];
 		this.socket = new WebSocket('ws://' + ip + ':' + port);
-		console.log('Created socket');
-		this.socket.onclose = socketOnDisconnect;
-		//this.socket.onerror = socketOnDisconnect;
-		this.socket.onopen = socketOnopen;
-		this.socket.onmessage = socketOnmessage;
+		this.socket.onclose = this.socketOnDisconnect.bind(this);
+		this.socket.onopen = this.socketOnopen.bind(this);
+		this.socket.onmessage = this.socketOnmessage.bind(this);
 	}
+};
+
+WebClient.Connection.prototype.setEncryptor = function(encryptor){
+	this.encryptor = encryptor;
+};
+
+WebClient.Connection.prototype.setDecryptor = function(decryptor){
+	this.decryptor = decryptor;
+};
+
+WebClient.Connection.prototype.sendBytes = function(array){
+	if(this.binaryQueue == null)
+		this.sendBytesNow(array);
+	else
+		this.binaryQueue.push(array);
+};
+
+WebClient.Connection.prototype.sendBytesNow = function(array){
+	this.socket.send(array.buffer);
+};
+
+WebClient.Connection.prototype.isOpen = function(){
+	return this.binaryQueue === null && !this.closed;
+};
+
+WebClient.Connection.prototype.close = function(){
+	this.socket.close();
+};
+
+WebClient.Connection.prototype.createOutput = function(initialCapacity){
+	const thisClient = this;
+	return new BitHelper.ByteArrayBitOutput(new Int8Array(initialCapacity || 100), 0, function(){
+		let bytes = this.getBytes();
+		if (thisClient.encryptor !== null){
+			bytes = thisClient.encryptor.encrypt(bytes);
+		}
+		thisClient.sendBytes(bytes);
+	});
+};
+
+WebClient.Connection.prototype.socketOnopen = function(event){
+	for(let index = 0; index < this.binaryQueue.length; index++){
+		this.sendBytesNow(this.binaryQueue[index]);
+	}
+	this.binaryQueue = null;
+	if(this.onOpen){
+		this.onOpen(event);
+	}
+};
+
+WebClient.Connection.prototype.socketOnmessage = function(event){
+	const fileReader = new FileReader();
+	const thisConnection = this;
+	fileReader.onload = function(event2) {
+		let bytes = new Int8Array(event2.target.result);
+		if (thisConnection.decryptor !== null){
+			bytes = thisConnection.decryptor.decrypt(bytes);
+		}
+		thisConnection.onMessage(new BitHelper.ByteArrayBitInput(bytes));
+	};
+	fileReader.readAsArrayBuffer(event.data);
+};
+
+WebClient.Connection.prototype.socketOnDisconnect = function(event){
+	if(this.isOpen()){
+		this.onDisconnect(event);
+	}
+	else if(onFailConnect){
+		this.onFailConnect(event);
+	}
+	this.closed = true;
 };
